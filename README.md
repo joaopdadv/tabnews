93b3ad28499a7ec4623954a9114224cc6a649ba2
João Pedro <joaopdadv@gmail.com>
2026-01-06 13:04:02 -0300

    adiciona prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
+
 Projeto de implementação fora a fora.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste de deploy automático com vercel</h1>
+    return <h1>Teste de deploy automático com vercel</h1>;
 }
 
 export default Home;
