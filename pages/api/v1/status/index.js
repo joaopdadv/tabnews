52969578f74fb4a1f45120b7bb69291e167918de
João Pedro <joaopdadv@gmail.com>
2026-01-27 14:38:36 -0300

    finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log(result.rows);
-    response.status(200).json({ chave: "teste de retorno da api joão" });
+    const updatedAt = new Date().toISOString();
+
+    const databaseVersionQuery = await database.query("SHOW server_version;");
+    const databaseVersion = databaseVersionQuery.rows[0].server_version;
+
+    const maxConnectionsQuery = await database.query("SHOW max_connections;");
+    const maxConnections = parseInt(
+        maxConnectionsQuery.rows[0].max_connections,
+    );
+
+    const dbName = process.env.POSTGRES_DB;
+    const openedConnectionsQuery = await database.query(
+        `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+        [dbName],
+    );
+    const openedConnections = openedConnectionsQuery.rows[0].count;
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: databaseVersion,
+                max_connections: maxConnections,
+                opened_connections: openedConnections,
+            },
+        },
+    });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,12 @@
 test("GET to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
-
     expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // garante que é uma data, mas não é 100%
+    expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+    expect(responseBody.dependencies.database.version).toBe("16.0");
+    expect(responseBody.dependencies.database.max_connections).toBe(100);
+    expect(responseBody.dependencies.database.opened_connections).toBe(1); // importante ser 1 nesse teste pois ao rodar uma query, apenas ela deve estar usando a conexão
 });
