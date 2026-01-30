e47b1e9421fbc7b2362f1e144f104b0351426bc2
João Pedro <joaopdadv@gmail.com>
2026-01-30 10:43:48 -0300

    adiciona ssl na conexão do banco

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,18 @@
 import { Client } from "pg";
 
 async function query(queryObject, params) {
-    const client = new Client({
+    const credentials = {
         host: process.env.POSTGRES_HOST,
         port: process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
-    });
+        ssl: process.env.NODE_ENV === "development" ? false : true,
+    };
 
-    console.log("Credenciais do postgres:", {
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-    });
+    const client = new Client(credentials);
+
+    console.log("Credenciais do postgres:", credentials);
 
     try {
         await client.connect();
